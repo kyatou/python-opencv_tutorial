f8d3886903f8c24dd94cf77005588e3f19700904
Kouji Yatou <kouji.yatou@gmail.com>
2012-07-12 18:40:04 +0900

    add cat image. edit04 for gaussian blur filter

--- 04_smooth.py
+++ 04_smooth.py
@@ -1,7 +1,6 @@
 '''
 python-opencv tutorial
 Create trackbar control.
-
 Usage:
 	04_smooth.py imagename
 '''
@@ -24,6 +23,15 @@ except:
 	print 'faild to load %s' % imagefilename
 	quit()
 
+#GaussianBlur(...)
+#    GaussianBlur(src, ksize, sigmaX[, dst[, sigmaY[, borderType]]]) -> dst
+# ksize must pair of odd. (5,5),(7,7),(9,9)...
+bluredimg=cv2.GaussianBlur(img,(11,11),0)
+
+cv2.imshow('Gaussian',bluredimg)
+cv2.imshow('Source',img)
+
+cv2.waitKey(0)
 
 
 cv2.destroyAllWindows() 
