37284a8633aff35799ae6c30e508805eeac9dd9e
Kouji Yatou <kouji.yatou@gmail.com>
2012-07-13 22:16:14 +0900

    add 07_image_roi
    Signed-off-by: Kouji Yatou <kouji.yatou@gmail.com>

--- 07_image_roi.py
+++ 07_image_roi.py
@@ -1,10 +1,10 @@
 '''
 python-opencv tutorial
-Smoothing image by gaussian filter.
+Create image ROI(Region of Interest)
 Usage:
-	04_smooth.py imagename
-
-  copyright Kouji Yatou <kouji.yatou@gmail.com>
+    07_image_roi.py imagename
+  
+   Copyright Kouji Yatou <kouji.yatou@gmail.com>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@ Usage:
    See the License for the specific language governing permissions and
    limitations under the License.
 
-
 '''
 
 #import opencv library
@@ -28,10 +27,9 @@ import numpy
 
 argvs=sys.argv
 if (len(argvs) != 2):
-    print 'Usage: # python %s filename' % argvs[0]
+    print 'Usage: # python %s imagefilename' % argvs[0]
     quit()
  
-
 imagefilename = argvs[1]
 try:
 	img=cv2.imread(imagefilename, 1)
@@ -39,20 +37,21 @@ except:
 	print 'faild to load %s' % imagefilename
 	quit()
 
-#GaussianBlur(...)
-#    GaussianBlur(src, ksize, sigmaX[, dst[, sigmaY[, borderType]]]) -> dst
-# ksize must pair of odd. (5,5),(7,7),(9,9)...
-bluredimg=cv2.GaussianBlur(img,(11,11),0)
+imgshape=img.shape
+roiWidth=imgshape[1]/4
+roiHeight=imgshape[0]/4
+sx=imgshape[1]/2-roiWidth
+sy=imgshape[0]/2-roiHeight
+ex=imgshape[1]/2+roiWidth
+ey=imgshape[0]/2+roiHeight
 
-cv2.imshow('Gaussian',bluredimg)
-cv2.imshow('Source',img)
+#extract roi as array
+roi=img[sy:ey,sx:ex]
 
+#invert roi area
+img[sy:ey,sx:ex]=cv2.bitwise_not(roi)
+
+cv2.imshow('roi image',img)
 cv2.waitKey(0)
-
-
 cv2.destroyAllWindows() 
-
-
-
-
 				
